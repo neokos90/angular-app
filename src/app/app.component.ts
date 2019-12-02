661ab9b29e211ef2563ed7a52329cc0ca5f2ec16
neokos <44771700+neokos90@users.noreply.github.com>
2019-12-02 17:13:29 +0800

    addition layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import {Menu} from './menu';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = 'Tour of Menues';
+  menuList =   [
+    new Menu(0, 'Все'),
+    new Menu(1, 'Любимые'),
+    new Menu(2, 'Удалённые'),
+  ];
+  myMenu = this.menuList[0];
 }
---
+++ src/app/menu.ts
@@ -0,0 +1,5 @@
+export class Menu {
+    constructor(
+        public id: number,
+        public name: string) {}
+}
